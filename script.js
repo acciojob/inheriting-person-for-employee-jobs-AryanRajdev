315d4ae8024021eddc440b866c1c90974e1c4b52
AryanRajdev <91838247+AryanRajdev@users.noreply.github.com>
2025-04-12 23:28:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,40 @@
-// Constructor function for Person
-function Person(name, age) {
-  this.name = name;
-  this.age = age;
-}
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Old School Classes</title>
+</head>
+<body>
+  <script>
+    // Constructor function for Person
+    function Person(name, age) {
+      this.name = name;
+      this.age = age;
+    }
 
-// Add method to Person's prototype
-Person.prototype.greet = function() {
-  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
-};
+    // Corrected log format for Cypress test
+    Person.prototype.greet = function () {
+      console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+    };
 
-// Constructor function for Employee (inherits from Person)
-function Employee(name, age, jobTitle) {
-  Person.call(this, name, age); // Call Person constructor with this context
-  this.jobTitle = jobTitle;
-}
+    // Constructor function for Employee (inherits from Person)
+    function Employee(name, age, jobTitle) {
+      Person.call(this, name, age);
+      this.jobTitle = jobTitle;
+    }
 
-// Set up inheritance: Employee.prototype inherits from Person.prototype
-Employee.prototype = Object.create(Person.prototype);
+    // Inherit from Person
+    Employee.prototype = Object.create(Person.prototype);
+    Employee.prototype.constructor = Employee;
 
-// Fix the constructor pointer (optional but good practice)
-Employee.prototype.constructor = Employee;
+    // Also match Cypress log expectations
+    Employee.prototype.jobGreet = function () {
+      console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+    };
 
-// Add method to Employee's prototype
-Employee.prototype.jobGreet = function() {
-  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
-};
+    // Make classes available on window for Cypress
+    window.Person = Person;
+    window.Employee = Employee;
+  </script>
+</body>
+</html>
 
-// Create instances
-const person = new Person("Alice", 25);
-person.greet();
-
-const employee = new Employee("Bob", 30, "Manager");
-employee.jobGreet();
